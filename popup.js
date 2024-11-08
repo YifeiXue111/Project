a748ee349395ac061611ec895f8684edb15b6b01
YifeiXue111 <156253841+YifeiXue111@users.noreply.github.com>
2024-11-08 12:40:26 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Button 1 event listener
-    document.getElementById('button1').addEventListener('click', function() {
+    document.getElementById('summarize all').addEventListener('click', function() {
         console.log('Button 1 clicked');
         // Add action for Button 1
     });
 
     // Button 2 event listener
-    document.getElementById('button2').addEventListener('click', function() {
+    document.getElementById('summarize selected').addEventListener('click', function() {
         console.log('Button 2 clicked');
         // Add action for Button 2
     });
